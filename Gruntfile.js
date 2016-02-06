6c46c713477ab027e543f5d95c88378d1740e3c6
Exin Coda <ggaao1994@gmail.com>
2016-02-06 22:49:11 +0800

    CRLF/LF?

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,13 +32,29 @@ module.exports = function(grunt) {
     },
     clean: {
       concated: ['dist/scripts.js', 'dist/controllers.js']
+    },
+    wiredep: {
+      task: {
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'app/index.html'
+        ],
+
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+          exclude: ['/angular-messages/']
+          // https://github.com/taptapship/wiredep#configuration
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-clean');
-
+  grunt.loadNpmTasks('grunt-wiredep');
   // 默认被执行的任务列表。
   grunt.registerTask('default', ['concat', 'uglify', 'clean']);
 }
